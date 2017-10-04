f97a0c337018b02253615ade9d845bacb6c90840
Willi Ballenthin <willi.ballenthin@gmail.com>
2017-10-04 17:53:53 -0400

    add init tests

---
+++ tests/test_emu.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+
+import unicorn
+
+import ucutils
+import ucutils.emu
+
+
+# x86-64::
+#
+#    0: mov rax, 0x1
+#    7: mov rbx, 0x2
+#    e: sub rbx, rax
+CODE = b'\x48\xC7\xC0\x01\x00\x00\x00\x48\xC7\xC3\x02\x00\x00\x00\x48\x29\xC3'
+
+
+def test_read_reg():
+    emu = ucutils.emu.Emulator(unicorn.UC_ARCH_X86, unicorn.UC_MODE_64)
+    emu.mem_map(0x0, 0x1000)
+    emu.mem_write(0x0, CODE)
+    emu.emu_start(0x0, len(CODE))
+    assert emu.rax == 0x1
+    assert emu.rbx == 0x1
+
+
+def test_write_reg():
+    emu = ucutils.emu.Emulator(unicorn.UC_ARCH_X86, unicorn.UC_MODE_64)
+    emu.rax = 0x1
+    assert emu.reg_read(unicorn.x86_const.UC_X86_REG_RAX) == 0x1
+
+    emu.pc = 0x7
+    assert emu.pc == 0x7
+    assert emu.rip == 0x7
+
+    emu.pc = 0x0
+    assert emu.pc == 0x0
+    assert emu.rip == 0x0
+
+
+def test_read_mem_slice():
+    emu = ucutils.emu.Emulator(unicorn.UC_ARCH_X86, unicorn.UC_MODE_64)
+    emu.mem_map(0x0, 0x1000)
+    emu.mem_write(0x0, CODE)
+    assert emu.mem[0x0:0x2] == b'\x48\xC7'
+
+
+def test_stepi():
+    emu = ucutils.emu.Emulator(unicorn.UC_ARCH_X86, unicorn.UC_MODE_64)
+    emu.mem_map(0x0, 0x1000)
+    emu.mem_write(0x0, CODE)
+    emu.stepi()
+    assert emu.pc == 0x7
+    assert emu.rax == 0x1
+
+
+def test_go():
+    emu = ucutils.emu.Emulator(unicorn.UC_ARCH_X86, unicorn.UC_MODE_64)
+    emu.mem_map(0x0, 0x1000)
+    emu.mem_write(0x0, CODE)
+    emu.go(0xE)
+    assert emu.pc == 0xE
+    assert emu.rax == 0x1
+    assert emu.rbx == 0x2
+
+
+class InsnCounter(ucutils.emu.Hook):
+    '''
+    counts the number of times the code tracing hook is invoked.
+    '''
+    HOOK_TYPE = unicorn.UC_HOOK_CODE
+
+    def __init__(self):
+        super(ucutils.emu.Hook, self).__init__()
+        self.count = 0
+
+    def hook(self, uc, address, size, user_data):
+        self.count += 1
+
+
+def test_hooks():
+    emu = ucutils.emu.Emulator(unicorn.UC_ARCH_X86, unicorn.UC_MODE_64)
+    emu.mem_map(0x0, 0x1000)
+    emu.mem_write(0x0, CODE)
+
+    c0 = InsnCounter()
+    c1 = InsnCounter()
+
+    with ucutils.emu.hook(emu, c0):
+        emu.stepi()
+
+        with ucutils.emu.hook(emu, c1):
+            emu.stepi()
+
+    # because we're single stepping, the counts are inflated by two.
+    # for each step, the hook is invoked for the instruct that gets executed,
+    # and then for the instruction that would be executed next. so there's double counting.
+    assert c0.count == 4
+    assert c1.count == 2
+
+
+def test_context():
+    emu = ucutils.emu.Emulator(unicorn.UC_ARCH_X86, unicorn.UC_MODE_64)
+    emu.mem_map(0x0, 0x1000)
+    emu.mem_write(0x0, CODE)
+
+    emu.rax = 0x0
+    emu.rbx = 0x0
+    emu.pc = 0x0
+
+    with ucutils.emu.context(emu):
+        emu.go(0xE)
+        assert emu.pc == 0xE
+        assert emu.rax == 0x1
+        assert emu.rbx == 0x2
+
+    assert emu.rax == 0x0
+    assert emu.rbx == 0x0
+    assert emu.pc == 0x0
---
+++ tests/test_x64.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import binascii
+
+import unicorn
+
+import ucutils
+import ucutils.emu
+
+
+def test_fs():
+    emu = ucutils.emu.Emulator(unicorn.UC_ARCH_X86, unicorn.UC_MODE_64)
+
+    emu.mem_map(0x0, 0x1000)     # code
+    emu.mem_map(0x1000, 0x1000)  # fs segment
+    emu.mem_map(0x2000, 0x1000)  # scratch
+
+    code = binascii.unhexlify(b'6448330C2518000000')  # x86-64: xor rcx, qword ptr fs:[0x18]
+    emu.mem_write(0x0, code)
+    emu.mem_write(0x1000+0x18, b'AAAAAAAA')
+
+    ucutils.arch.x64.set_fs(emu, 0x1000, scratch=0x2000)
+    assert ucutils.arch.x64.get_fs(emu, scratch=0x2000) == 0x1000
+
+    emu.emu_start(0x0, len(code))
+
+    assert emu.rcx == 0x4141414141414141
+
+
+def test_gs():
+    emu = ucutils.emu.Emulator(unicorn.UC_ARCH_X86, unicorn.UC_MODE_64)
+
+    emu.mem_map(0x0, 0x1000)     # code
+    emu.mem_map(0x1000, 0x1000)  # gs segment
+    emu.mem_map(0x2000, 0x1000)  # scratch
+
+    code = binascii.unhexlify(b'6548330C2518000000')  # x86-64: xor rcx, qword ptr gs:[0x18]
+    emu.mem_write(0x0, code)
+    emu.mem_write(0x1000+0x18, b'AAAAAAAA')
+
+    ucutils.arch.x64.set_gs(emu, 0x1000, scratch=0x2000)
+    assert ucutils.arch.x64.get_gs(emu, scratch=0x2000) == 0x1000
+
+    emu.emu_start(0x0, len(code))
+
+    assert emu.rcx == 0x4141414141414141
+
+
+def test_ptr():
+    emu = ucutils.emu.Emulator(unicorn.UC_ARCH_X86, unicorn.UC_MODE_64)
+    emu.mem_map(0x0, 0x1000)
+    ucutils.arch.x64.emit_ptr(emu, 0x0, 0x1122334455667788)
+
+    assert emu.mem[0x0:0x8] == b'\x88\x77\x66\x55\x44\x33\x22\x11'
+    assert ucutils.arch.x64.parse_ptr(emu, 0x0) == 0x1122334455667788
